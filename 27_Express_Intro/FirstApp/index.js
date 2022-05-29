d32532aa3574f2278aa39f79a04e53c05b562fe9
Adam A <adamaubry2362@gmail.com>
2022-05-29 05:57:02 -0500

    Creating Dynamic HTML With Templating

---
+++ 27_Express_Intro/FirstApp/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const res = require("express/lib/response");
+const app = express()
+
+// app.use((req, res) => {
+//     console.log("WE GOT A NEW REQUEST")
+//     // res.send("HELLO WE GOT YOUR REQUEST!")
+//     // res.send({color: 'red'})
+//     // res.send('<h1> This is my webpage</h1>')
+// })
+app.listen(8080, () => {
+    console.log("LISTENING ON PORT 8080!")
+})
+app.get('/', (req,res) => {
+    res.send('This is the homepage!!!')
+})
+app.get('/r/:subreddit', (req, res) => {
+    const {subreddit} = req.params;
+    res.send(`<h1>Browsing the ${subreddit} subreddit</h1>`)
+})
+app.get('/r/:subreddit/:postId', (req, res) => {
+    const {subreddit, postId} = req.params;
+    res.send(`<h1>Viewing post ID ${postId} Browsing the ${subreddit} subreddit</h1>`)
+})
+app.post('./cats', (req,res) => {
+    res.send('POST REQUEST TO /cats!!! THIS IS NOT A GET REQUEST')
+})
+
+app.get('/cats', (req, res) => {
+    res.send('MEOW!!')
+})
+
+app.get('/dogs', (req, res) => {
+    res.send('Ruff!!')
+})
+
+app.get('/search', (req, res) => {
+    const {q} = req.query;
+    if(!q){
+        res.send("NOTHING FOUND IF NOTHING SEARCHED")
+    }
+    res.send(`<h1>Search results for: ${q}</h1>`)
+})
+
+app.get('*', (req,res) => {
+    res.send(`I DONT KNOW THAT PATH`)
+})
+
---
+++ 28_Templating/Templating_Demo/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const { get } = require('express/lib/response');
+const app = express();
+const path = require('path');
+const redditData = require('./data.json')
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'))
+
+app.get('/',(req,res) => {
+    res.render('home.ejs')
+})
+
+app.get('/cats',(req,res)=>{
+    const cats = [
+        'Jimmy', 'Ricky', 'George', 'Donny', 'blake'
+    ]
+    res.render('cats', {cats})
+})
+
+app.get('/r/:subreddit', (req,res)=> {
+    const {subreddit} = req.params;
+    const data = redditData[subreddit];
+    if(data){
+        res.render('subreddit', {...data});
+    }else{
+        res.render('notfound', {subreddit})
+    }
+})
+
+app.get('/rand', (req,res)=> {
+    const num = Math.floor(Math.random() * 10)+1
+    res.render('random', {rand: num})
+})
+
+app.listen(8080, () => {
+    console.log("LISTENING ON PORT 8080")
+})
